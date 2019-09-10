c8e71027a1b9934346f2756e8b90729dff96b8cb
jay vyas <jayunit100.github@gmail.com>
2019-09-10 13:27:32 -0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Get the secret for image pull from Jeremy/Senthil/Jay/...
 kubectl create -f secret.yml
 /usr/local/bin/synopsysctl create polaris native default --version test --pull-secret gcr-json-key --postgres-password admin --postgres-username admin --environment-dns onprem.dev.polaris.synopsys.com --environment-name onprem  --smtp-host mailhost.internal.synopsys.com --smtp-port 25 --smtp-username root --smtp-password root| kubectl apply -f - -n default --validate=false
 
